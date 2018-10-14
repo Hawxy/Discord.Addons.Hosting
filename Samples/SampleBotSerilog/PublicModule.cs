7e808f2733517a1329e7e9ee24e471744efd3b3f
Hawx <Hawxy@users.noreply.github.com>
2018-10-14 12:06:23 +0800

    Move stuff around

---
+++ Samples/SampleBotSerilog/PublicModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace SampleBotSerilog
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        //will be injected
+        public ILogger<PublicModule> _logger { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public async Task PingAsync()
+        {
+            _logger.LogInformation($"User {Context.User.Username} used the ping command!");
+            await ReplyAsync("pong!");
+        }
+
+        [Command("log")]
+        public async Task TestLogs()
+        {
+            _logger.LogTrace("This is a trace log");
+            _logger.LogDebug("This is a debug log");
+            _logger.LogInformation("This is an information log");
+            _logger.LogWarning("This is a warning log");
+            _logger.LogError(new InvalidOperationException("Invalid Operation"), "This is a error log with exception");
+            _logger.LogCritical(new InvalidOperationException("Invalid Operation"), "This is a critical load with exception");
+
+            _logger.Log(GetLogLevel(LogSeverity.Error), "Error logged from a Discord LogSeverity.Error");
+            _logger.Log(GetLogLevel(LogSeverity.Info), "Information logged from Discord LogSeverity.Info ");
+        }
+
+        private static LogLevel GetLogLevel(LogSeverity severity)
+            => (LogLevel)(Math.Abs((int)severity - 5));
+    }
+}
